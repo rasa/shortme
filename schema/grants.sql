d4e74a19cbbffb9d6e9d7402618d7cb4fb59e664
Ross Smith II <ross@smithii.com>
2018-11-05 22:21:29 +0000

    Fix grants.sql

--- schema/grants.sql
+++ schema/grants.sql
@@ -1,5 +1,5 @@
 -- see https://github.com/andyxning/shortme#grant
-grant insert, delete on sequence.* to 'sequence'@'%' identified by 'sequence';
-grant insert on shortme.* to 'shortme_w'@'%' identified by 'shortme_w';
+grant select, insert, update, delete on sequence.* to 'sequence'@'%' identified by 'sequence';
+grant insert, update on shortme.* to 'shortme_w'@'%' identified by 'shortme_w';
 grant select on shortme.* to 'shortme_r'@'%' identified by 'shortme_r';
 
